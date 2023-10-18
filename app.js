58b07d7c97a4b59145af94f52266851d2dd5b049
aliamini1422 <aliaminii1422@gmail.com>
2023-10-18 21:34:22 +0330

    changed pdf

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -10,3 +11,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hide');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const pageWrapper = document.querySelector('#wrapper');
+
+
+document.querySelector('.moon').addEventListener('click', (e)=>{
+    pageWrapper.classList.toggle('dark')
+    console.log(pageWrapper)
+
+})
